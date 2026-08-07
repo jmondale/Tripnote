24cdd74b7d1246b49924f6c8aa35987e990380ee
Jaye Mondale <jmondale@users.noreply.github.com>
2026-08-07 12:52:39 -0600

    Initial Commit

---
+++ Tripnote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tripnote
+//
+//  Created by Jaye Mondale on 8/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tripnote/TripnoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripnoteApp.swift
+//  Tripnote
+//
+//  Created by Jaye Mondale on 8/7/26.
+//
+
+import SwiftUI
+
+@main
+struct TripnoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
